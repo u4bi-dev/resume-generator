9598ff03064e86c79c06c4f2bfa4a2c78b480851
u4bi <u4bi@u4bi.com>
2017-09-07 15:30:27 +0900

    ProjectSection.ts render

---
+++ src/template/ProjectSection.ts
@@ -0,0 +1,31 @@
+export default function ProjectSection(data) {
+    
+    function item(){
+        
+        let result = '';
+
+        data.map( (data) => {
+            
+            let html = `
+                <div>
+                    <h3>${ data.name }</a></h3>
+                    <h4>${ data.date.start } &mdash; ${ data.date.end }</h4>
+                    <p>${ data.bio }</p>
+                </div>
+            `;
+
+            result += html;         
+        });
+
+        return result;
+    }
+    
+    return `
+        <section>
+        <header>
+            <h2>Projects</h2>
+        </header>
+        ${ item() }
+        </section>    
+    `;
+}

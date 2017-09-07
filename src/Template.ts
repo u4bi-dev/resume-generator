43d299d79b55838a5325f1123d2e5e7f80884df3
u4bi <u4bi@u4bi.com>
2017-09-07 15:29:44 +0900

    Template.ts render

---
+++ src/Template.ts
@@ -0,0 +1,29 @@
+import Style from './template/Style';
+import Header from './template/Header';
+import Footer from './template/Footer';
+import ProjectSection from './template/ProjectSection';
+import SkillSection from './template/SkillSection';
+import ExperienceSection from './template/ExperienceSection';
+import EduAndCertSection from './template/EduAndCertSection';
+
+export default function Template(data) {
+  
+    return `
+        ${ Style() }
+        <div class="wrapper">
+          <!-- 상단 헤더 -->
+          ${ Header(data.header) }
+          <!-- 프로젝트 섹션 -->
+          ${ ProjectSection(data.projects) }
+          <!-- 기술 섹션 -->
+          ${ SkillSection(data.skills) }
+          <!-- 경험 섹션 -->
+          ${ ExperienceSection(data.experience) } 
+          <!-- 교육 및 인증 섹션 -->
+          ${ EduAndCertSection(data.eduandcert) }
+          <!-- 하단 푸터 -->
+          ${ Footer(data.footer) }
+
+        </div>        
+        `;
+}

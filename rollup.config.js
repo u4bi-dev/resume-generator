ecdb40abc183c54796e60d9170b1fd64ce4f6a05
u4bi <u4bi@u4bi.com>
2017-09-07 12:14:40 +0900

    라이브러리 네임 변경
    - resume - > Resume

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from 'rollup-plugin-typescript';
 export default {
     entry: './src/App.ts',
     format: 'umd',
-    moduleName: 'resume',
+    moduleName: 'Resume',
     // sourceMap: 'inline',
     dest: 'dist/resume.js',
     plugins: [

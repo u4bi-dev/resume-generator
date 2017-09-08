e9733b8026befc43523a1eb9c644fe3203df8c68
u4bi <u4bi@u4bi.com>
2017-09-08 16:06:14 +0900

    BuilderStyle.ts render

---
+++ src/template/BuilderStyle.ts
@@ -0,0 +1,55 @@
+export default function BuilderStyle(){
+           
+    return `
+    <style>
+        .builder-wrapper{
+            position:fixed;       
+            bottom:0;
+            right:0;
+            color: #fff;
+            text-align:center;
+            width: 50px;
+            height: 50px;
+            -webkit-transition:all .2s;
+                    transition:all .2s;            
+        }
+
+        .builder-wrapper:hover{
+            background-color: rgba(0,0,0,0.6);
+            -webkit-box-shadow: 2px 2px 2px 2px #000;            
+                    box-shadow: 2px 2px 2px 2px #000;   
+            width: 220px;
+            height: 110px;                     
+        }
+
+        .builder-json{
+            font-weight: 550;
+            line-height: 55px;
+            -webkit-transition:all 0.2s ease;
+                    transition:all 0.2s ease;
+            width:90%;
+        }
+        .builder-json:hover{
+            background-color:rgba(0,0,0,0.7);
+            cursor: pointer;
+        }
+
+        .builder-wrapper input{ 
+            color:#fff;
+            border: 0;
+            outline:none;
+        }
+        
+        .builder-json,
+        .builder-wrapper input{
+            margin:auto;
+            margin-top:10px;
+            border-radius : 3px;
+            background-color: #333;
+            text-decoration: none;                        
+        }
+                            
+    </style>      
+    `;
+
+}
